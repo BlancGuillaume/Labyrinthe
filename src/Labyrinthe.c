9858e193739dd53655c7d7480da9010746cc77f0
guillaume <guillaume.blanc.etudiant@gmail.com>
2015-04-03 21:58:01 +0200

    Test Premier commit

---
+++ src/Labyrinthe.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Labyrinthe.c
+ Author      : Guillaume Blanc
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!! Ca marcheeeeeeeeeee!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
